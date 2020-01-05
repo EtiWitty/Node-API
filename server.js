3683ebc2dd3db9328167583b7c882cd2f1a9e655
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-01-05 15:25:04 -0800

    made server work by parsing the port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res, next) => {
 	res.send('Hello from Node server');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.listen (PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
