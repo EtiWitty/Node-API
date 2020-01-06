bb8188a0dea687d6347a0c81b0645151350a1d82
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-01-05 16:00:02 -0800

    implemented err handling middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ dotEnv.config();
 
 const app = express();
 
+// Application-level middleware
 const myMiddleware = (req, res, next) => {
 	console.log("Hey What's up?");
 	next();
@@ -20,4 +21,14 @@ const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.listen (PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
+});
+
+// error-handler middleware
+app.use((err, req, res, next) => {
+	console.error(ee.stack);
+	res.status(500).send({
+		status: 500,
+		message:err.message,
+		body: {}
+	});
 });
